f0401ab219a3253400a50db591e2fb18c896ac95
Farid Seifi <farid.seifi@gmail.com>
2023-09-25 14:54:22 -0400

    lint formating applied. Deno.Run replaced with Deno.Command

--- deps.ts
+++ deps.ts
@@ -1,4 +1,3 @@
-
 export * as path from "https://deno.land/std/path/mod.ts";
 export { isURL } from "https://deno.land/x/is_url/mod.ts";
-export {existsSync} from "https://deno.land/std/fs/mod.ts";
+export { existsSync } from "https://deno.land/std/fs/mod.ts";
--- manifest.ts
+++ manifest.ts
@@ -1,26 +1,30 @@
 /// <reference lib="deno.ns" />
-import { path, isURL, existsSync  } from "./deps.ts";
-import {getFilenameFromURL, isLocalPathOrURL, download, unzip, resolveRelativeURL} from "./load.ts"
+import { existsSync, isURL, path } from "./deps.ts";
+import {
+  download,
+  getFilenameFromURL,
+  isLocalPathOrURL,
+  resolveRelativeURL,
+  unzip,
+} from "./load.ts";
 
-let manifest_path =""
-let collection_path ="./pyshelf"
-
+let manifest_path = "";
+let collection_path = "./pyshelf";
 
 /**
-   * reads and returns content of manifet file
-   */
-async  function get_Manifest(){ //works with remote or local path (relative or absolute)
+ * reads and returns content of manifet file
+ */
+async function get_Manifest() { //works with remote or local path (relative or absolute)
   try {
-    if(isLocalPathOrURL(manifest_path)){ //local manifest
+    if (isLocalPathOrURL(manifest_path)) { //local manifest
       const fileContent = await Deno.readFile(manifest_path);
       const text = new TextDecoder().decode(fileContent);
       const jsonData = JSON.parse(text);
       return jsonData;
-    }
-    else{ //remote manifest
+    } else { //remote manifest
       const response = await fetch(manifest_path);
       if (response.status === 200) {
-        const json = await response.json();        
+        const json = await response.json();
         return json;
       } else {
         console.error(`Failed to fetch JSON. Status code: ${response.status}`);
@@ -32,50 +36,61 @@ async  function get_Manifest(){ //works with remote or local path (relative or a
   return {};
 }
 
-
 /**
-   * loads ko at itemLocation, downloads and unzips if needed 
-   */
-async function loadKO(itemLocation: string){
-
+ * loads ko at itemLocation, downloads and unzips if needed
+ */
+async function loadKO(itemLocation: string) {
   //resolve itemLocation
-  if(!path.isAbsolute(itemLocation) && !isURL(itemLocation)){ //if itemLocation is relative, need to resolve towards manifest path
-    if(isLocalPathOrURL(manifest_path)) itemLocation=resolveRelativeURL(path.toFileUrl(manifest_path).href,itemLocation); //make manifest path a uri so that it could be resolved
-    else  itemLocation=resolveRelativeURL(manifest_path,itemLocation)
-  } 
+  if (!path.isAbsolute(itemLocation) && !isURL(itemLocation)) { //if itemLocation is relative, need to resolve towards manifest path
+    if (isLocalPathOrURL(manifest_path)) {
+      itemLocation = resolveRelativeURL(
+        path.toFileUrl(manifest_path).href,
+        itemLocation,
+      ); //make manifest path a uri so that it could be resolved
+    } else itemLocation = resolveRelativeURL(manifest_path, itemLocation);
+  }
 
   //Download to localLocationif not local
-  let localLocation=itemLocation;
+  let localLocation = itemLocation;
   try {
-    if(!isLocalPathOrURL(itemLocation)){
-      localLocation=collection_path + "/" + getFilenameFromURL(itemLocation);
-      await download(itemLocation, localLocation);   
+    if (!isLocalPathOrURL(itemLocation)) {
+      localLocation = collection_path + "/" + getFilenameFromURL(itemLocation);
+      await download(itemLocation, localLocation);
+    } else {
+      localLocation = path.fromFileUrl(localLocation);
     }
-    else{
-      localLocation=path.fromFileUrl(localLocation)
-    }  
 
     //unzip if local file exist and ko does not exist in cache
-    const cacheFolder=collection_path + "/" + getFilenameFromURL(itemLocation).replace(path.extname(itemLocation),"");
-    if(existsSync(localLocation) && !existsSync(cacheFolder)){
-      await unzip(localLocation,collection_path);
+    const cacheFolder = collection_path + "/" +
+      getFilenameFromURL(itemLocation).replace(path.extname(itemLocation), "");
+    if (existsSync(localLocation) && !existsSync(cacheFolder)) {
+      await unzip(localLocation, collection_path);
     }
   } catch (error) {
-    console.error("Error loading ko at ",itemLocation, ". error: ", error.message);
+    console.error(
+      "Error loading ko at ",
+      itemLocation,
+      ". error: ",
+      error.message,
+    );
   }
 }
 
 async function main() {
-  collection_path=Deno.env.get("ORG_KGRID_JAVASCRIPT_ACTIVATOR_COLLECTION_PATH") ?? "";
+  collection_path =
+    Deno.env.get("ORG_KGRID_JAVASCRIPT_ACTIVATOR_COLLECTION_PATH") ?? "";
   if (collection_path == "" || undefined) {
-    collection_path="./pyshelf"
+    collection_path = "./pyshelf";
   }
 
-  manifest_path=Deno.env.get("ORG_KGRID_JAVASCRIPT_ACTIVATOR_MANIFEST_PATH") ?? "";
+  manifest_path =
+    Deno.env.get("ORG_KGRID_JAVASCRIPT_ACTIVATOR_MANIFEST_PATH") ?? "";
   if (manifest_path == "" || undefined) {
     console.log(`ORG_KGRID_JAVASCRIPT_ACTIVATOR_MANIFEST_PATH is not defined.`);
   } else {
-    if(!path.isAbsolute(manifest_path) && isLocalPathOrURL(manifest_path)) manifest_path=path.resolve(manifest_path); //used to support relative manifest path
+    if (!path.isAbsolute(manifest_path) && isLocalPathOrURL(manifest_path)) {
+      manifest_path = path.resolve(manifest_path); //used to support relative manifest path
+    }
     console.log(manifest_path);
 
     try {
@@ -91,4 +106,3 @@ async function main() {
 }
 
 main();
-
